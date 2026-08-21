e5e9df2da4bde0bf481f7735156404caec07a089
vedant <vedantchougale5@gmail.com>
2026-08-21 21:15:16 +0530

    Time: 0 ms (100%), Space: 8.3 MB (50.83%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string interpret(string command) {
+       string ans="";
+        for(int i=0;i<command.length();i++){
+            if(command[i]=='G'){
+                ans+="G";
+            }
+            else if (command[i]=='(' && command[i+1]==')'){
+                ans+="o";
+            }
+            else if(command[i]=='(' && command[i+1]=='a' && command[i+2]=='l' && command[i+3]==')'){
+                ans+="al";
+            }
+        }
+        return ans;
+    }
+};
